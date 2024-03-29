03f8af91a003c5885130380a7312950617caa8b1
ReshamVishwakarma <reshamofficial123@gmail.com>
2024-03-29 20:55:32 +0530

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/img1.jpg";
+import image from "../images/resham.png";
 
 const imageAltText = "This is Resham Vishwakram- Full Stack Web Developer. This is my Background image. ";
 
